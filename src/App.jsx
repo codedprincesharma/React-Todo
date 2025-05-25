cc2541e00d3c5d426011804c48d66d6cd3ada49a
Prince Sharma <hit.princeraj@gmail.com>
2025-05-26 02:43:41 +0530

    radio

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Todo() {
+  const [todo, setTodo] = useState([
+    { id: 1, title: "kaam kar le bhai", isCompleted: false },
+  ]);
 
+  const [title, setTitle] = useState("");
+  const [Check, setCheck] = useState(true);
+  const [gender, setgender] = useState("female");
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Create Task</h1>
+      <form action="">
+        <input
+          onChange={(e) => title.log(e.target.value)}
+          value={setTitle}
+          type="text"
+          placeholder="title"
+        />
+        <br />
+        <br />
+        <input
+          checked={Check}
+          onChange={(e) => setCheck.log(e.target.checked)}
+          type="checkbox"
+          name=""
+          id=""
+        />
+        Completed <br />
+        <br />
+        <button>Create Todo</button>
+        <input
+          value={gender}
+          onChange={(e) => setgender.log(e.target.value)}
+          checked={gender == "male" && true}
+          type="radio"
+        />
+        male
+        <input
+          value={gender}
+          onChange={(e) => setgender.log(e.target.value)}
+          checked={gender == "female" && true}
+          type="radio"
+        />
+        Female
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default Todo;
